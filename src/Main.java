8b889f82b40bfde8895f3548a6e8c062e7fa537e
Sfirlea Alina <alinasfirlea23@yahoo.com>
2017-11-10 18:20:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double distantakm=300;
+        double ore=2;
+        double viteza=(distantakm*1000)/(ore*3600);
+        System.out.println("viteza broscutei este : " +viteza +"m/s");
+
+    }
+}
